ea1798382cc0d10207e66a020aff262a073dbce6
M4rtono <35830105+M4rtono@users.noreply.github.com>
2018-02-15 22:42:34 +0700

    tambah evt.consume() pada lib textarea

--- GudangGaramL/src/KomponenGUI/JTextAreaF.java
+++ GudangGaramL/src/KomponenGUI/JTextAreaF.java
@@ -1,10 +1,22 @@
 package KomponenGUI;
 
+import static KomponenGUI.JtextF.MaxText;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JTextArea;
 
 public class JTextAreaF extends JTextArea {
 
     public JTextAreaF() {
         setFont(new java.awt.Font("Tahoma", 0, 18));
+        setLineWrap(true);
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent evt) {
+                if (evt.getKeyCode() == KeyEvent.VK_ENTER) {
+                    evt.consume();
+                }
+            }
+        });
     }
 }
